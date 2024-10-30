e2930ac8a2f8c9df287246109069af2e9092cb29
basma <jamaleddinebassmajamaleddine@gmail.come>
2024-10-30 16:16:14 +0100

    2f

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    
+    $users=User::where('tfa_enable', 1)->get();
+    foreach ($users as $user) {
+        $code = random_int(1000, 9999);
+        $user->tfa_code =  $code;
+        $user->save();
+    }
+   
+})->everyMinute();
